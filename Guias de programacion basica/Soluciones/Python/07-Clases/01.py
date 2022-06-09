e3939fee8084bbef300737719850dd6fa42313b7
Vincent <vdepassier@outlook.com>
2022-06-09 16:37:01 -0400

    clases exercices

--- Guias de programacion basica/Soluciones/Python/07-Clases/01.py
+++ Guias de programacion basica/Soluciones/Python/07-Clases/01.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun  9 10:49:11 2022
+Created on Thu Jun  9 16:07:39 2022
 
 @author: vince
 """
-
 class Pokemon:
     def __init__(self, nombre, tipo, vida, ataque, velocidad):
         self.nombre = nombre
@@ -12,46 +11,59 @@ class Pokemon:
         self.vida = vida
         self.ataque = ataque
         self.velocidad = velocidad
-    
-    def atacar(self): # getter
-        return self.ataque * 0.2
-    
-    def recibirAtaque(self, ataquePokemon):
-        self.vida -= ataquePokemon
-        return self.vida
-    
+        
     def __str__(self):
         return f"Pokemon: {self.nombre} / {self.tipo} / {self.vida} / {self.ataque} / {self.velocidad}"
+    
+    def atacar(self):
+        return self.ataque * 0.2
+    
+    def recibirAtaque(self, ataqueRecibido):
+        self.vida -= ataqueRecibido
+        return self.vida
 
 
 def main():
     print("Data Pokemon: nombre, tipo, vida, ataque, velocidad")
-    stringData1 = input("Ingrese data Pokemon 1: ")
-    stringData2 = input("Ingrese data Pokemon 2: ")
-    listaData1 = stringData1.split(" ")
-    listaData2 = stringData2.split(" ")
-    pk1 = Pokemon(listaData1[0], listaData1[1], int(listaData1[2]), int(listaData1[3]), int(listaData1[4]))
-    pk2 = Pokemon(listaData2[0], listaData2[1], int(listaData2[2]), int(listaData2[3]), int(listaData2[4]))
-    
+    stringPk1 = input("Ingrese data pokemon1: ")
+    stringPk2 = input("Ingrese data pokemon2: ")
+    print("")
+    datapk1 = stringPk1.split(" ")
+    datapk2 = stringPk2.split(" ")
+    pk1 = Pokemon(datapk1[0], datapk1[1], int(datapk1[2]), int(datapk1[3]), int(datapk1[4]))
+    pk2 = Pokemon(datapk2[0], datapk2[1], int(datapk2[2]), int(datapk2[3]), int(datapk2[4]))
+    print(pk1)
+    print(pk2)
+    print("")
     turno = 0
     if(pk1.velocidad > pk2.velocidad):
         turno = 1
     else:
         turno = 2
-    
+        
     while(pk1.vida > 0 and pk2.vida > 0):
         if(turno == 1):
             turno = 2
-            print(f"{pk1.nombre}[{pk1.vida}] ataca a {pk2.nombre}[{pk2.vida}] / {pk2.nombre} pierde {pk1.atacar()} hp [{pk2.recibirAtaque(pk1.atacar())}] ")
+            pokeAtaque = round(pk1.atacar(), 1)
+            pokeVidaAtacado = round(pk2.recibirAtaque(pk1.atacar()), 1)
+            vida1 = round(pk1.vida, 1)
+            vida2 = round(pk2.vida, 1)
+            print(f"{pk1.nombre}[{vida1}] ataca a {pk2.nombre}[{vida2}] / {pk2.nombre} pierde {pokeAtaque} hp [{pokeVidaAtacado}]")
         else:
             turno = 1
-            print(f"{pk2.nombre}[{pk2.vida}] ataca a {pk1.nombre}[{pk1.vida}] / {pk1.nombre} pierde {pk2.atacar()} hp [{pk1.recibirAtaque(pk2.atacar())}] ")
-    
+            pokeAtaque = round(pk2.atacar(), 1)
+            pokeVidaAtacado = round(pk1.recibirAtaque(pk2.atacar()), 1)
+            vida1 = round(pk1.vida, 1)
+            vida2 = round(pk2.vida, 1)
+            print(f"{pk2.nombre}[{vida2}] ataca a {pk1.nombre}[{vida1}] / {pk1.nombre} pierde {pokeAtaque} hp [{pokeVidaAtacado}]")
+            
+    print("")
     if(pk1.vida > 0):
-        print(f"{pk1.nombre} gana el combate contra {pk2.nombre}, quedando con {pk1.vida} hp")
+        vida1 = round(pk1.vida, 1)
+        print(f"{pk1.nombre} gana el combate contra {pk2.nombre}, quedando con {vida1} hp")
     else:
-        print(f"{pk2.nombre} gana el combate contra {pk1.nombre}, quedando con {pk2.vida} hp")
-
-
-if(__name__ == "__main__"):
+        vida2 = round(pk2.vida, 1)
+        print(f"{pk2.nombre} gana el combate contra {pk1.nombre}, quedando con {vida2} hp")
+        
+if __name__ == "__main__":
     main()
